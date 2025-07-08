25d7467600d55a1bba478f7245653b71371248cf
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2025-07-08 12:13:15 +0300

    update modals

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -2,6 +2,7 @@ class Modal {
   constructor() {
     this.openButtons = document.querySelectorAll('[data-modal-open]');
     this.closeButtons = document.querySelectorAll('[data-modal-close]');
+    this.generateButtons = document.querySelectorAll('[data-modal-generate]');
     this.title = '';
     this.content = '';
     this.selector = '';
@@ -21,11 +22,36 @@ class Modal {
           this.content = e.currentTarget.dataset.modalContent;
           this.selector = e.currentTarget.dataset.modalClass;
 
-          this.open(modal, this.title, this.content, this.selector);
+          this.open(modal);
         } catch (error) {
           console.error(
             `Ошибка, окно не найдено: ${e.currentTarget.dataset.modalOpen} \n ${error}`
           );
+        }
+      });
+    });
+
+    this.generateButtons.forEach((generateButton) => {
+      generateButton.addEventListener('click', (e) => {
+        try {
+          const existModal = document.querySelector(
+            '[data-modal-id="' + e.currentTarget.dataset.modalGenerate + '"]'
+          );
+
+          this.title = e.currentTarget.dataset.modalTitle;
+          this.content = e.currentTarget.dataset.modalContent;
+          this.selector = e.currentTarget.dataset.modalClass;
+
+          if (
+            existModal &&
+            e.currentTarget.dataset.modalGenerate === existModal.dataset.modalId
+          ) {
+            this.open(existModal);
+          } else {
+            this.generate(e);
+          }
+        } catch (error) {
+          console.error(error);
         }
       });
     });
@@ -55,16 +81,50 @@ class Modal {
     });
   }
 
-  open(modal, title, content, selector) {
-    const modalTitle = modal?.querySelector('.modal-title');
-    const modalContent = modal?.querySelector('.modal-body');
-
+  open(modal) {
+    this.setup(modal);
     modal.classList.add('show');
     document.documentElement.classList.add('modal-open');
+  }
 
-    if (title) modalTitle.innerHTML = title;
-    if (content) modalContent.innerHTML = content;
-    if (selector) modal?.classList.add(selector);
+  generate(e) {
+    const modal = document.createElement('div');
+
+    modal.innerHTML = `
+            <button class="btn btn-close" data-modal-close>
+              <i class="icon-cross"></i>
+            </button>
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-title h2"></div>
+                <div class="modal-body"></div>
+              </div>
+            </div>
+          `;
+
+    modal.classList.add('modal');
+    modal.dataset.modalId = e.currentTarget.dataset.modalGenerate;
+
+    document.body.append(modal);
+
+    setTimeout(() => {
+      this.open(modal);
+    }, 50);
+  }
+
+  setup(modal, title, content, selector) {
+    if (modal) {
+      const modalTitle = modal?.querySelector('.modal-title');
+      const modalContent = modal?.querySelector('.modal-body');
+
+      title ? (this.title = title) : null;
+      content ? (this.content = content) : null;
+      selector ? (this.selector = selector) : null;
+
+      modalTitle.innerHTML = this.title;
+      modalContent.innerHTML = this.content;
+      modal?.classList.add(this.selector);
+    }
   }
 
   close(modal) {
